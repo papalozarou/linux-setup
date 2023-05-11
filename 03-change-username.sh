b672f7cba9cbc3826da704859e0441c80210ec60
papalozarou <github@lozworld.com>
2023-05-11 13:33:28 +0000

    Edited temp user check, removed extra spaces

--- 03-change-username.sh
+++ 03-change-username.sh
@@ -2,7 +2,7 @@
 
 #-------------------------------------------------------------------------------
 # Changes the current user's username by:
-# 
+#
 # 1. Creating a temporary user, `tempUser`;
 # 2. Creating a script within their home directory to change the default ubuntu
 #    username and groupname; and
@@ -12,10 +12,10 @@
 #-------------------------------------------------------------------------------
 # Create a temporary user, `tempUser`, and adds it to sudoers so it can change
 # the current users name and group.
-# 
+#
 # N.B.
-# The `adduser` command uses `--gecos GECOS` to so that it only asks for a 
-# password for the new user during setup, rather than all the other information, 
+# The `adduser` command uses `--gecos GECOS` to so that it only asks for a
+# password for the new user during setup, rather than all the other information,
 # as per:
 #
 # https://unix.stackexchange.com/a/611219
@@ -23,7 +23,7 @@
 createTempUser () {
   echo "$COMMENT_PREFIX"'Creating temporary user, tempUser.'
   echo "$COMMENT_PREFIX"'N.B.'
-  echo "$COMMENT_PREFIX"'Set a password you can remember easily as you will need it shortly.'  
+  echo "$COMMENT_PREFIX"'Set a password you can remember easily as you will need it shortly.'
   adduser --gecos GECOS tempUser
   echo "$COMMENT_PREFIX"'Temporary user created. Adding to sudoers.'
   adduser tempUser sudo
@@ -52,7 +52,7 @@ usermod -d /home/$NEW_USER -m $NEW_USER
 groupmod --new-name $NEW_USER $SUDO_USER
 echo "$COMMENT_PREFIX""You can now log back in as $NEW_USER."
 echo "$COMMENT_PREFIX""Once logged in run 03-change-username.sh again."
-EOF  
+EOF
 
   echo "$COMMENT_PREFIX"'Script created. Please log in as tempUser and run the script with:'
   echo "$COMMENT_PREFIX"'./renameUser.sh.'
@@ -69,21 +69,22 @@ removeTempUser () {
 }
 
 #-------------------------------------------------------------------------------
-# Check whether `tempUser` exists. If it doesn't exist, create it, or if it does 
+# Check whether `tempUser` exists. If it doesn't exist, create it, or if it does
 # exist, delete it.
 #-------------------------------------------------------------------------------
 checkForTempUser () {
-  local tempUser=echo $(cat /etc/passwd | grep tempUser) 
+  local TEMPUSER=echo $(cat /etc/passwd | grep tempUser)
 
-  if [ -z tempUser]; then
-    removeTempUser
-  else
+  if [ -z TEMPUSER ]; then
     createTempUser
     getNewUserName
     createTempUserScript
     setPermissions "+x" "/home/tempUser/renameUser.sh"
     setOwner "tempUser" "/home/tempUser/renameUser.sh"
     echo "$COMMENT_PREFIX"'You can now log this user out, log in as tempUser, then run ./renameUser.sh.'
+  else
+    removeTempUser
+  fi
 }
 
 #-------------------------------------------------------------------------------
