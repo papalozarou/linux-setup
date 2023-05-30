5dc59a71e59d9aa8afbdec08accd9522783501aa
papalozarou <github@lozworld.com>
2023-05-30 19:16:46 +0000

    Added main function

--- 07-configure-ufw.sh
+++ 07-configure-ufw.sh
@@ -13,5 +13,23 @@
 . ./00-shared-functions.sh
 
 #-------------------------------------------------------------------------------
+# Main script that checks to see if this step has been completed before. If so
+# exit, if not run the setup for this step.
+#-------------------------------------------------------------------------------
+configureUfw () {
+  local STEP_COMPLETED=$(checkSetupConfigOption configuredUfw)
+
+  echo "$COMMENT_PREFIX"'Checking '"$SETUP_CONF"' to see if UFW has already been configured.'
+
+  if [ $STEP_COMPLETED = true ]; then
+    echo "$COMMENT_PREFIX"'UFW already configured. No setup required.'
+    echoScriptExiting
+  else
+    echo "$COMMENT_PREFIX"'UFW not configured. Starting setup…'
+  fi
+}
+
+#-------------------------------------------------------------------------------
 # Run the script.
 #-------------------------------------------------------------------------------
+configureUfw
