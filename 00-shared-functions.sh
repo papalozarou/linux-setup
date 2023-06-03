23215564bb715a0ac0e9ef87268ec4832472d7a2
papalozarou <github@lozworld.com>
2023-06-03 18:54:12 +0000

    Corrected typo

--- 00-shared-functions.sh
+++ 00-shared-functions.sh
@@ -232,10 +232,10 @@ initialiseScript () {
   echo "$COMMENT_PREFIX"'Checking '"$SETUP_CONF"' to see if this step has already been performed.'
   echo "$COMMENT_PREFIX"'Check returned '"$CONFIG_KEY_TF"'.'
 
-  if [ $CONFIG_KEY = "true" ]; then
+  if [ $CONFIG_KEY_TF = true ]; then
     echo "$COMMENT_PREFIX"'You have already performed this step.'
     echoScriptExiting
-  elif [ $CONFIG_KEY = "false" ]; then
+  elif [ $CONFIG_KEY_TF = false ]; then
     echo "$COMMENT_PREFIX"'You have not performed this step. Running script.'
     echo "$COMMENT_SEPARATOR"
     runScript
