575a3de274fb5d84074b7461f072afca627dda84
papalozarou <github@lozworld.com>
2023-07-08 22:54:17 +0000

    Added correct rm statement

--- 06-configure-sshd.sh
+++ 06-configure-sshd.sh
@@ -73,7 +73,7 @@ removeCurrentSshdConfigs () {
 
   if [ "$SSHD_CONFS_YN" = 'y' -o "$SSHD_CONFS_YN" = 'Y' ]; then
     echoComment "Deleting files in $SSHD_CONF_DIR."
-    sh -c 'rm "$SSHD_CONF_DIR/*.conf"'
+    sh -c "rm $SSHD_CONF_DIR/*.conf"
     echoComment 'Files deleted.'
 
     listCurrentSshdConfigs
