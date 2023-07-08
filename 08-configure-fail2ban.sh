fde447d437eff7e3ab6242b19bda567112dda879
papalozarou <github@lozworld.com>
2023-07-08 12:28:38 +0000

    Refactored configure-fail2ban

--- 08-configure-fail2ban.sh
+++ 08-configure-fail2ban.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
 #-------------------------------------------------------------------------------
-# Install and configure fail2ban. The configuration is stored at
+# Install and configure fail2ban by:
+# 
+# 1. checking if fail2ban is installed, installs if not;
+# 2. generating the hardened config file;
+# 3. adjusting permissions of the config file;
+# 4. starting fail2ban; and
+# 5. listing the current status of fail2ban.
+# 
+# The configuration is stored at:
+# 
 # /etc/fail2ban/jail.local
 #
 # N.B.
-# This script needs to be run as `sudo`.
+# We check for `fail2ban-server`, not `fail2ban`.
+# 
+# This script needs to be run as "sudo".
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
@@ -14,18 +25,24 @@
 . ./00-shared-functions.sh
 
 #-------------------------------------------------------------------------------
+# Config key and service variables.
+#-------------------------------------------------------------------------------
+CONFIG_KEY='configuredFail2ban'
+SERVICE="$(changeCase "${CONFIG_KEY#'configured'}" "lower")"
+
+#-------------------------------------------------------------------------------
 # fail2ban related variables.
 #-------------------------------------------------------------------------------
-GLOBAL_FAIL2BAN_DIR=/etc/fail2ban
-FAIL2BAN_DEFAULT_CONF=$GLOBAL_FAIL2BAN_DIR/jail.local
+GLOBAL_FAIL2BAN_DIR='/etc/fail2ban'
+FAIL2BAN_DEFAULT_CONF="$GLOBAL_FAIL2BAN_DIR/jail.local"
 
 #-------------------------------------------------------------------------------
 # Creates the hardened config file for fail2ban. This overides the default 
 # values stored in /etc/fail2ban/jail.conf.
 #-------------------------------------------------------------------------------
 createHardenedFail2banConfig () {
-  echo "$COMMENT_PREFIX"'Generating fail2ban config file at '"$FAIL2BAN_DEFAULT_CONF"'.'
-  cat <<EOF > $FAIL2BAN_DEFAULT_CONF
+  echoComment "Generating fail2ban config file at $FAIL2BAN_DEFAULT_CONF."
+  cat <<EOF > "$FAIL2BAN_DEFAULT_CONF"
 [DEFAULT]
 ignoreip = 127.0.0.1/8
 bantime  = 10m
@@ -35,39 +52,26 @@ maxretry = 3
 [sshd]
 enabled	= true
 EOF
-  echo "$COMMENT_PREFIX"'Config file generated.'
+  echoComment 'Config file generated.'
 }
 
 #-------------------------------------------------------------------------------
-# Runs if this step hasn't been completed before. The script:
-#
-# 1. checks if fail2ban is installed, installs if not;
-# 2. generates the hardened config file;
-# 3. adjusts permissions of the config file;
-# 4. starts fail2ban; and
-# 5. lists the current status of fail2ban.
-# 
-# N.B.
-# We check for `fail2ban-server`, not `fail2ban`.
+# Runs the main functions of the script.
 #-------------------------------------------------------------------------------
-runScript () {
-  local SERVICE='fail2ban'
-
-  checkForServiceAndInstall $SERVICE
+mainScript () {
+  checkForServiceAndInstall "$SERVICE"
 
   createHardenedFail2banConfig
-  setPermissions 644 $FAIL2BAN_DEFAULT_CONF
+  setPermissions '644' "$FAIL2BAN_DEFAULT_CONF"
 
-  controlService start $SERVICE
+  controlService 'start' "$SERVICE"
 
-  controlService status $SERVICE
-
-  writeSetupConfigOption configuredFail2ban true
-
-  echoScriptFinished "setting up $SERVICE."
+  controlService 'status' "$SERVICE"
 }
 
 #-------------------------------------------------------------------------------
-# Performs the initial check to see if this step has already been completed.
+# Run the script.
 #-------------------------------------------------------------------------------
-initialiseScript configuredFail2ban
+initialiseScript "$CONFIG_KEY"
+mainScript
+finaliseScript "$CONFIG_KEY"
