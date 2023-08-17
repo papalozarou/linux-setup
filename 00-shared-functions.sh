2331f5d3de4e48cc0ff52e5448972bc8671049a4
papalozarou <github@lozworld.com>
2023-08-17 11:10:19 +0100

    Switched returned values

--- 00-shared-functions.sh
+++ 00-shared-functions.sh
@@ -163,9 +163,9 @@ checkForDirectory () {
   local DIR="${1:?}"
 
   if [ -d "$DIR" ]; then
-    echo false
-  else
     echo true
+  else
+    echo false
   fi
 }
 
@@ -179,9 +179,9 @@ checkForFile () {
   local FILE="${1:?}"
 
   if [ -f "$FILE" ]; then
-    echo false
-  else
     echo true
+  else
+    echo false
   fi
 }
 
