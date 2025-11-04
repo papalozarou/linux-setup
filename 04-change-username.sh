a8129e663f6ec83927374cfb128a9384043df6da
papalozarou <github@lozworld.com>
2025-11-04 23:09:28 +0000

    Updated script generation to remove variable errors when script is created

--- 04-change-username.sh
+++ 04-change-username.sh
@@ -103,27 +103,25 @@ createTempUser () {
 # sudoers include file if present.
 #-------------------------------------------------------------------------------
 createTempUserScript () {
+  local SUDOERS_OVERRIDE_PATH="/etc/sudoers.d/010_pi-nopasswd"
+  
   printComment 'Creating a script to change the current username and group within the "tempuser" home directory at:'
   printComment "$RENAME_SCRIPT_PATH"
 
   cat <<EOF > "$RENAME_SCRIPT_PATH"
 #!/bin/sh
-OLD_USER="$SUDO_USER"
-MEW_USER="$NEW_USER"
-SUDOERS_PATH="/etc/sudoers.d/010_pi-nopasswd"
+echo "$COMMENT_PREFIX Changing username and group of the user $SUDO_USER."
+usermod -l $NEW_USER $SUDO_USER
+usermod -d /home/$NEW_USER -m $SUDO_USER
+groupmod --new-name $NEW_USER $SUDO_USER
+echo "$COMMENT_PREFIX Username and group changed from $SUDO_USER to $NEW_USER."
 
-echo "$COMMENT_PREFIX Changing username and group of the user $OLD_USER."
-usermod -l $NEW_USER $OLD_USER
-usermod -d /home/$NEW_USER -m $OLD_USER
-groupmod --new-name $NEW_USER $OLD_USER
-echo "$COMMENT_PREFIX Username and group changed from $OLD_USER to $NEW_USER."
-
-if [ -f "\$SUDOERS_PATH" ]; then
-  sed -i "s/^\$OLD_USER[[:space:]]\+ALL=(ALL) NOPASSWD: ALL\$/$NEW_USER ALL=(ALL) NOPASSWD: ALL/" "\$SUDOERS_PATH"
+if [ -f "\$SUDOERS_OVERRIDE_PATH" ]; then
+  sed -i "s/^\$SUDO_USER[[:space:]]\+ALL=(ALL) NOPASSWD: ALL\$/$NEW_USER ALL=(ALL) NOPASSWD: ALL/" "\$SUDOERS_OVERRIDE_PATH"
   echo "$COMMENT_SEPARATOR"
-  grep $NEW_USER $SUDOERS_PATH
+  grep $NEW_USER $SUDOERS_OVERRIDE_PATH
   echo "$COMMENT_SEPARATOR"
-  echo "$COMMENT_PREFIX Updated sudoers file: $SUDOERS_PATH"
+  echo "$COMMENT_PREFIX Updated sudoers file: $SUDOERS_OVERRIDE_PATH"
 fi
 
 echo "$COMMENT_PREFIX You can now log back in as the user $NEW_USER, using the existing password."
